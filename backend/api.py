c8bda1c73dd0b429e36c254f11c406e8dba908c7
Niclas Sieveneck <niclass95@live.de>
2025-04-18 14:00:56 +0200

    Cursor Connection in a try block

--- backend/api.py
+++ backend/api.py
@@ -86,9 +86,13 @@ def taco_test():
 
 @taco.route('/tacodb', methods=['GET'])
 def taco_test_db():
-    cur.execute("SELECT name FROM taco_stack_test LIMIT 1")
-    result = cur.fetchall()
-    result_string = "\n".join([str(row[0]) for row in result])
+    try:
+        cur.execute("SELECT name FROM taco_stack_test LIMIT 1")
+        result = cur.fetchall()
+        result_string = "\n".join([str(row[0]) for row in result])
+    except mariadb.Error as e:
+        result_string = f"Error connecting to MariaDB Platform: {e}"
+        print(f"Error connecting to MariaDB Platform: {e}")
     return result_string
 
 
