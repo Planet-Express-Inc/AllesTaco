db104617bd32337f125cf7bdbf4350624269fbb1
Niclas Sieveneck <niclass95@live.de>
2025-06-03 20:07:44 +0200

    Article views added

--- backend/routes/v1/__init__.py
+++ backend/routes/v1/__init__.py
@@ -3,5 +3,6 @@ from .article import article_bp
 from .status import status_bp
 from .cart import cart_bp
 from .user_purchase import user_purchase_bp
+from .article_views import article_views_bp
 
-v1_blueprints = [user_bp, article_bp, status_bp, cart_bp, user_purchase_bp]
+v1_blueprints = [user_bp, article_bp, status_bp, cart_bp, user_purchase_bp, article_views_bp]
---
+++ backend/routes/v1/article_views.py
@@ -0,0 +1,43 @@
+from flask import Blueprint, jsonify
+
+from functions import *
+
+article_views_bp = Blueprint('article_views', __name__, url_prefix='/v1')
+
+
+### Views for aricles
+@article_views_bp.route('/article/views/<artikel_id>', methods=['GET','POST','REMOVE'])
+@article_views_bp.route('/article/views', methods=['GET','POST'])
+def article_views(artikel_id=None):
+
+    # Get info
+    if request.method == 'GET':
+        result = execute_query("SELECT aufrufer_id, artikel_id, anzahl FROM aufrufe WHERE artikel_id=?", [artikel_id])
+        return jsonify(result), 200
+    
+    # Add new
+    if request.method == 'POST':
+        json_data = request.get_json()
+
+        # Add aricle_id
+        if artikel_id is not None:
+            json_data["artikel_id"] = artikel_id
+            
+        needed_parameters = ["artikel_id", "anzahl"]
+
+        data = json_exctract_and_validate(json_data, needed_parameters)
+
+        if not data:
+            return jsonify(default_error), 405
+        
+        data = sort_parameters(data, needed_parameters)
+        if(execute_edit("INSERT INTO aufrufe(artikel_id, anzahl) VALUES (?, ?)", data)):
+            return jsonify(default_ok), 201
+        
+        return jsonify(default_error), 405
+    
+    # Delete
+    if request.method == 'DELETE':
+        if(execute_edit("DELETE FROM aufrufe WHERE artikel_id=?", [artikel_id])):
+            return jsonify(default_ok), 204
+        return jsonify(default_error), 405
