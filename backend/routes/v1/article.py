2dfa9a715dbb14098df1ca5ad98d1526ec733695
Niclas Sieveneck <niclass95@live.de>
2025-06-03 20:13:07 +0200

    Refactored article.py

--- backend/routes/v1/article.py
+++ backend/routes/v1/article.py
@@ -12,35 +12,33 @@ def article(article_id=None):
     # Get info
     if request.method == 'GET':
         result = execute_query("SELECT artikel_id, titel, verkaeufer_id, beschreibung, preis, status, bestand, kategorie FROM artikel WHERE artikel_id=?", [article_id])
-        return result
+        return jsonify(result), 200
     
+    # Auth after that
+    if not check_login():
+       return jsonify(default_error_no_login), 403
+
     # Add new
     if request.method == 'POST':
-        if not check_login():
-            return default_error_no_login
-        
         json_data = request.get_json()
         needed_parameters = ["titel", "verkaeufer_id", "beschreibung", "preis", "bild", "status", "bestand", "kategorie"]
         data = json_exctract_and_validate(json_data, needed_parameters)
 
         if not data:
-            return default_error
+            return jsonify(default_error), 405
         
         data = sort_parameters(data, needed_parameters)
         execute_edit("INSERT INTO artikel(titel, verkaeufer_id, beschreibung, preis, bild, status, bestand, kategorie) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", data)
-        return default_ok
+        return jsonify(default_ok), 201
     
     # Delete
     if request.method == 'DELETE':
-        if not check_login():
-            return default_error_no_login
-        
         execute_edit("DELETE FROM artikel WHERE artikel_id=?", [article_id])
-        return default_ok
+        return jsonify(default_ok), 204
 
 # Picture for article
 # TODO: Post for Picure? Or via JSON (-> Article POST)
 @article_bp.route('/article/picture/<article_id>', methods=['GET'])
 def get_article_picture(article_id):
     result = download_data("SELECT bild FROM artikel WHERE artikel_id=?", [article_id], article_id)
-    return result
+    return jsonify(result), 200
