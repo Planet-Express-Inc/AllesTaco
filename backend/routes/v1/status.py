081eb5c89803c01e6b397da5e3fcf328cd55c964
Niclas Sieveneck <niclass95@live.de>
2025-06-11 21:55:55 +0200

    Status renewal

--- backend/routes/v1/status.py
+++ backend/routes/v1/status.py
@@ -16,9 +16,9 @@ def taco_test_db():
     try:
         result_string = execute_query("SELECT name FROM taco_stack_test LIMIT 1", [])
         result_string = str(result_string[0]["name"]).replace('"','')
-        return jsonify(result_string), 200
+        return result_string, 200
     except mariadb.Error as e:
         result_string = f"Error connecting to MariaDB Platform: {e}"
         print(f"Error connecting to MariaDB Platform: {e}")
         print(result_string)
-    return jsonify(result_string), 500
+    return result_string, 500
