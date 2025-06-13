1b76e8a0dfaff0a99384ed37c1143b413c617e5a
Niclas Sieveneck <niclass95@live.de>
2025-06-13 18:08:37 +0200

    Cleanup purchases

--- backend/routes/v1/user_purchase.py
+++ backend/routes/v1/user_purchase.py
@@ -24,26 +24,6 @@ def purchase():
         result = execute_query("SELECT kauf_id, kaeufer_id, artikel_id, versanddaten, kaufpreis FROM abgeschlossene_kaeufe WHERE kaeufer_id=?", [session['username']])
         return jsonify(result), 200
     
-    # Old one
-    """
-    # Add new
-    if request.method == 'POST':
-        json_data = request.get_json()
-        needed_parameters = ["kaeufer_id", "artikel_id", "versanddaten", "kaufpreis"]
-        # Add user id
-        json_data["kaeufer_id"] = session['username']
-
-        data = json_exctract_and_validate(json_data, needed_parameters)
-
-        if not data:
-            return jsonify(default_error), 405
-        
-        data = sort_parameters(data, needed_parameters)
-        if(execute_edit("INSERT INTO abgeschlossene_kaeufe(kaeufer_id, artikel_id, versanddaten, kaufpreis) VALUES (?, ?, ?, ?)", data)):
-            return jsonify(default_ok), 201
-        
-        return jsonify(default_error), 405
-    """
     # New One
     if request.method == 'POST':
         # Get Shopping cart
@@ -97,11 +77,3 @@ def purchase():
             return jsonify(default_ok), 204
         return jsonify(default_error), 405
 
-
-"""
-
-@user_purchase_bp.route('/user/purchase/cart', methods=['GET'])
-def purchase_cart():
-    
-""" 
-    
