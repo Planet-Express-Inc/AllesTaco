809af86b80ec2db1adcbb632124d384c97a5ead8
Niclas Sieveneck <niclass95@live.de>
2025-06-13 12:21:20 +0200

    added cart

--- backend/routes/v1/cart.py
+++ backend/routes/v1/cart.py
@@ -7,25 +7,35 @@ cart_bp = Blueprint('cart', __name__, url_prefix='/v1')
 
 ### Cart
 # TODO: all, Swagger!
-@cart_bp.route('/cart', methods=['GET','POST','DELETE'])
-def cart():
+@cart_bp.route('/cart/<article_id>', methods=['DELETE'])
+@cart_bp.route('/cart', methods=['GET','POST'])
+def cart(article_id=None):
     if not check_login():
         return jsonify(default_error_no_login), 403
     
-    # session['username']
-    
     if request.method == 'GET':
-        result = execute_query("SELECT warenkorb_id FROM artikel WHERE kaeufer_id=?", session['username'])
-        print("cart-get: ")
-        print(result)
-        #####
+        result = execute_query("SELECT benutzer_id, artikel_id, anzahl, verkaeufer_id FROM warenkorb WHERE benutzer_id=?", [session['username']])
         return jsonify(result), 200
 
+    # Add new
     elif request.method == 'POST':
-        data = request.json
-        return jsonify({"message": "POST-Methode", "data": data}), 201
+        json_data = request.get_json()
+        needed_parameters = ["benutzer_id", "artikel_id", "anzahl", "verkaeufer_id"]
+
+        # Add user id
+        json_data["kaeufer_id"] = session['username']
+
+        data = json_exctract_and_validate(json_data, needed_parameters)
+
+        if not data:
+            return jsonify(default_error), 405
+        
+        data = sort_parameters(data, needed_parameters)
+        execute_edit("INSERT INTO warenkorb(benutzer_id, artikel_id, anzahl, verkaeufer_id) VALUES (?, ?, ?, ?)", data)
+        return jsonify(default_ok), 201
 
     elif request.method == 'DELETE':
-        return jsonify({"message": "DELETE-Methode ausgeführt"}), 204
+        execute_edit("DELETE FROM warenkorb WHERE artikel_id=?", [article_id])
+        return jsonify(default_ok), 204
 
     return default_error_not_sup, 405
