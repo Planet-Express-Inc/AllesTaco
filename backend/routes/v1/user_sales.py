fd3fef9f52a47c1c033267252da8388ffe335243
Niclas Sieveneck <niclass95@live.de>
2025-06-17 12:15:31 +0200

    Missing amount for pruchases or sales

--- backend/routes/v1/user_sales.py
+++ backend/routes/v1/user_sales.py
@@ -11,5 +11,5 @@ def sales(user_id):
     if not check_login():
         return jsonify(default_error_no_login), 403
 
-    result = execute_query("SELECT kauf_id, kaeufer_id, artikel_id, verkaeufer_id, versanddaten, kaufpreis FROM abgeschlossene_kaeufe WHERE verkaeufer_id=?", [user_id])
+    result = execute_query("SELECT kauf_id, kaeufer_id, artikel_id, anzahl, verkaeufer_id, versanddaten, kaufpreis FROM abgeschlossene_kaeufe WHERE verkaeufer_id=?", [user_id])
     return jsonify(result), 200
