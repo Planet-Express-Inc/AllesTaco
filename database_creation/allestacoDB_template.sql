5333f9c99eb741574d855b875c9d9a71892d3af2
Niclas Sieveneck <niclass95@live.de>
2025-06-17 12:06:59 +0200

    Removes minetype from db

--- database_creation/allestacoDB_template.sql
+++ database_creation/allestacoDB_template.sql
@@ -22,7 +22,6 @@ CREATE TABLE artikel (
     beschreibung TEXT,
     preis DECIMAL(10,2) NOT NULL,
     bild MEDIUMBLOB,
-    minetype VARCHAR(255),
     status ENUM('verfügbar', 'verkauft') NOT NULL,
     bestand INT NOT NULL,
     kategorie VARCHAR(255),
